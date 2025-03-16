0a2430373d68cb07b918a9eb075397f338bcadd2
Navneet Kumar <navnitkumar123@gmail.com>
2025-03-16 20:11:44 +0530

    updated project

--- src/component/Firstcomp.jsx
+++ src/component/Firstcomp.jsx
@@ -1,7 +1,7 @@
  const Firstcomp = ({ children }) => {
     return (
       <div className="w-96 p-6 bg-white shadow-lg rounded-lg border">
-        <h2 className="text-lg font-semibold mb-4">Anjali TwInle little star I &#x2665;&#xfe0f; you</h2>
+        <h2 className="text-lg font-semibold mb-4">Anjali Twinkle Twinkle little star I &#x2665;&#xfe0f; you</h2>
         {children}
       </div>
     );
