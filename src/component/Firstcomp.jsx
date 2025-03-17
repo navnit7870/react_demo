ebf356f8dae4642c25d113a62d5117a0bd5c58b0
Navneet Kumar <navnitkumar123@gmail.com>
2025-03-17 10:53:29 +0530

    updated project

--- src/component/Firstcomp.jsx
+++ src/component/Firstcomp.jsx
@@ -1,7 +1,7 @@
  const Firstcomp = ({ children }) => {
     return (
       <div className="w-96 p-6 bg-white shadow-lg rounded-lg border">
-        <h2 className="text-lg font-semibold mb-4">User Form</h2>
+        <h2 className="text-lg font-semibold mb-4">Login Form</h2>
         {children}
       </div>
     );
